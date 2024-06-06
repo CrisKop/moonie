baf237b4e311660f8e817c53ca19a3d99bea390c
CrisKop <cristianosprince@gmail.com>
2024-06-06 04:36:34 +0000

    Db and discord at start

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {user, pass} = process.env
 
 export const connectDB = async () => {
    try {
-    await mongoose.connect("mongodb+srv://cluster0-mjsvu.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true, user: "", pass: "", dbName: 'realtimechat'})
+    await mongoose.connect("mongodb+srv://cluster0-mjsvu.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true, user, pass, dbName: 'realtimechat'})
     console.log(">>> Db connected")
    } catch (error) {
     console.log(error);
